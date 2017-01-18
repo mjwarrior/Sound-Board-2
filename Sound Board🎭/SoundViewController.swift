98d55a010d55194444a902692d364a44b7e5fc67
marcstreeter <mstreeter@gmail.com>
2017-01-18 11:12:20 -0600

    I finished lecture 88 section 8 proceeding to lecture 89 have some errors gonna fix that i’m trying to make the record button do an action to record

--- Sound Board🎭/SoundViewController.swift
+++ Sound Board🎭/SoundViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
+import AVFoundation
+
 class SoundViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
   
     @IBOutlet weak var recordButton: UIButton!
     
+    var audioRecorder : AVAudioRecorder?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       setupRecorder()
     }
 
+    func setupRecorder() {
+   
+    
+    //Create an audio session
+    
+   let session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        session.overrideOutputAudioPort(.speaker)
+        session.setActive(true)
+        
+    // Create URL for the audio file
+         
+    // Create settiongs for the audio recorder
+        
+    // Create AudioRecorder object
+   
+    audioRecorder = AVAudioRecorder(url: <#T##URL#>, settings: <#T##[String : Any]#>)
+    
+    }
+    
+    
     @IBAction func recordTapped(_ sender: Any) {
     }
     
